615f09f6ec4f57c4253e6cbacd3e75b1b2fe3b27
ShotaLaabb <52139226+ShotikoLabadze@users.noreply.github.com>
2023-04-27 02:00:52 +0400

    add login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "firebase/app";
+import Login from "./components/Login";
 
 function App() {
-  return <div style={{ fontFamily: "Avenir" }}>shota</div>;
+  return (
+    <div style={{ fontFamily: "Avenir" }}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const auth = firebase
+  .initializeApp({
+    apiKey: "AIzaSyDeqMQOT3OOAEl_R6rQQ_xmYsO8czfGlDo",
+    authDomain: "mychat-a0b62.firebaseapp.com",
+    projectId: "mychat-a0b62",
+    storageBucket: "mychat-a0b62.appspot.com",
+    messagingSenderId: "772124469780",
+    appId: "1:772124469780:web:bfe569718375174a8d9501",
+  })
+  .auth();
